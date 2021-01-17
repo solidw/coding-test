6544151be1bb9842228924961b3140abe082eedc
solidw <solidw@naver.com>
2021-01-18 03:43:25 +0900

    docs: Add sort1-2750.js

---
+++ baekjoon/workbook/sort1/2750.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function solution(input) {
+  const N = input[0];
+  const numbers = input.slice(1);
+
+  numbers.sort((a, b) => a - b);
+
+  const answer = numbers.join('\n');
+  return answer;
+}
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  const answer = solution(input);
+  console.log(answer);
+  process.exit();
+});
